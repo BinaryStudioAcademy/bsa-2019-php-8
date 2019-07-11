931bacb54b3076a766f42fb0a00a9c0585deb49b
Oleksandr Strakhov <mrallexus@gmail.com>
2019-07-11 13:39:20 +0300

    Add auth exist test

---
+++ tests/Browser/Task1/AuthExistTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Browser\Task1;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class AuthExistTest extends DuskTestCase
+{
+    const USER_PASSWORD = 'secret';
+
+    public function testMainPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertSeeLink('Login')
+                ->assertSeeLink('Register')
+                ->clickLink('Login')
+                ->assertPathIs('/login')
+                ->back()
+                ->clickLink('Register')
+                ->assertPathIs('/register');
+        });
+    }
+
+    public function test_login()
+    {
+        $this->browse(
+            function (Browser $browser) {
+                $user = TestDataFactory::createUser();
+                $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', self::USER_PASSWORD)
+                    ->press('Login')
+                    ->assertPathIs('/currencies')
+                    ->assertDontSeeLink('Login');
+            }
+        );
+    }
+
+    public function test_registration()
+    {
+        $this->browse(
+            function (Browser $browser) {
+                $browser->visit('/register')
+                    ->assertSee('Register')
+                    ->type('name', 'Will Smith')
+                    ->type('email', 'will@example.com')
+                    ->value('input[name=password]', self::USER_PASSWORD)
+                    ->value('input[name=password_confirmation]', self::USER_PASSWORD)
+                    ->press('Register')
+                    ->assertPathIs('/products')
+                    ->assertDontSeeLink('Register');
+            }
+        );
+        $this->assertDatabaseHas('users', [
+            'name' => 'Will Smith',
+            'email' => 'will@example.com',
+        ]);
+    }
+}
---
+++ tests/Browser/Task1/MainPageTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Browser\Task1;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class MainPageTest extends DuskTestCase
+{
+    public function testMainPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Marketplace');
+        });
+    }
+}
